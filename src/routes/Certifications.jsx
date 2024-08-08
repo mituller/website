2a645e83d1cf439f7bf2cbc3151a4fb706403ee0
Michael Tuller <mituller@gmail.com>
2024-08-08 18:55:13 -0500

    favicon page title addition among other updates

--- src/routes/Certifications.jsx
+++ src/routes/Certifications.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import { useEffect } from "react";
+import CPACCLogo from '../assets/cpacc-logo.png';
+import WASLogo from '../assets/was-logo.png';
+import CPWALogo from '../assets/cpwa-logo.png';
+import '../styles/certifications.css';
 
 function Certifications() {
+    useEffect(() => {
+        document.title = "Michael Tuller - Certifications";
+      }, []);
     return (
-        <div>
-            <h1>Certifications</h1>
-            <p>Over the years I have done a great deal of work in accessibility. Like most, at first it was learn as you go and figuring out with searches. The basics were covered and used tools like Wave and use of screen readers to help find issues during development.</p>
-            <p>As time went by and my career developed, I was pulled more and more into accessibility solutioning, both in code and design. Tools grew and accessibility began to be looked at more critically and careers in the space developed. Because of my interest in this area, and the belief that this is where I can give something back, I decided to get my A11y certifications.</p>
-        </div>
+        <>
+            <div className="grid-x">
+                <h1>Certifications</h1>
+                <p>Over the years I have done a great deal of work in accessibility. Like most, at first it was learn as you go and figuring out with searches. The basics were covered and used tools like Wave and use of screen readers to help find issues during development.</p>
+                <p>As time went by and my career developed, I was pulled more and more into accessibility solutioning, both in code and design. Tools grew and accessibility began to be looked at more critically and careers in the space developed.</p>
+                <p>In 2020 I was fortunate to start working at U.S. Bank in a role that pushed me further on the accessibility path. The bank has a strong accessibility culture, and because of that extensive training through Deque University is manditory for all developers and designers. This gave me an even stronger foundation in accessibility and introduced me to more people that focused on A11y in the design space. I grew more interested, and with this decided to get my certifications in web accessibility.</p>
+                <p>I completed the Certified Professional in Web Accessibility (CPWA), which is attained after completing Certified Professional in Accessibility Core Competencies and the Web Accessibility Specialist certifications by the International Association of Accessibility Professionals.</p>
+            </div>
+
+            <div className="grid-x grid-padding-x align-center-middle text-center">
+                <div className="cell medium-4">
+                    <img className="iaap-logo" src={CPACCLogo} alt="Certified Professional in Accessibility Core Competencies logo" />
+                </div>
+                <div className="cell medium-4">
+                    <img className="iaap-logo" src={WASLogo} alt="Web Accessibility Specialist logo" />
+                </div>
+                <div className="cell medium-4">
+                    <img className="iaap-logo" src={CPWALogo} alt="Certified Professional in Web Accessibility logo" />
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import logo from '../assets/logo.png';
 function Home() {
+    useEffect(() => {
+        document.title = "Michael Tuller - Home";
+      }, []);
     return (
         <>
             <div className="grid-x" >
@@ -17,12 +20,14 @@ function Home() {
                         </ul>
                     </nav>
                 </div>
-                <main className="cell">
+                <main className="cell text-padding">
                     <h1>Welcome</h1>
-                    <p>I started my career as a designer and developer. I have worked in a number of areas within digital content and have always been drawn to connecting technology and people.I have seen the struggles that my parents, my peers, and my sons have had with digital information, and have always felt that there are better ways.
-                    </p>
-                    <p>Starting as a designer, I became facinated with how people percieved information. Later in my education I studied Psychology and Human Factors to understand deeper how everything connects. Then in 2013 my youngest son was in a car accident and suffered a severe TBI. Obviously this would change anyone's perspective, and since then I have been working with him to help get past the limitations that the accident put on him. In that journey, I found my passion in ensuring that technology is available to everyone. We just need to reach them.</p>
-                    <p>I hope you take a look at some of the work that I have included here. What you see is only the beginning.</p>
+                    <section >
+                        <p>I started my career as a designer and developer. I have worked in a number of areas within digital content and have always been drawn to connecting technology and people.I have seen the struggles that my parents, my peers, and my sons have had with digital information, and have always felt that there are better ways.
+                        </p>
+                        <p>Starting as a designer, I became facinated with how people percieved information. Later in my education I studied Psychology and Human Factors to understand deeper how everything connects. Then in 2013 my youngest son was in a car accident and suffered a severe TBI. Obviously this would change anyone's perspective, and since then I have been working with him to help get past the limitations that the accident put on him. In that journey, I found my passion in ensuring that technology is available to everyone. We just need to reach them.</p>
+                        <p>I hope you take a look at some of the work that I have included here. What you see is only the beginning.</p>
+                    </section>
                 </main>
             </div>
         </>
